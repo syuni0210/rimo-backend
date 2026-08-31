2265dea73e6f380b39138d029c8bc2fb8f608c5c
immaker99 <imsuhyun99@gmail.com>
2026-08-31 16:31:58 +0900

    feat: add safety facility map API

---
+++ data-api/src/main/java/com/ansim/backend/dto/FacilityMapDto.java
@@ -0,0 +1,103 @@
+package com.ansim.backend.dto;
+
+public class FacilityMapDto {
+
+    private Long id;
+
+    private String type;
+
+    private String name;
+
+    private String address;
+
+    private Double lat;
+
+    private Double lng;
+
+
+    public FacilityMapDto() {
+    }
+
+
+    public FacilityMapDto(
+            Long id,
+            String type,
+            String name,
+            String address,
+            Double lat,
+            Double lng
+    ) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(
+            Long id
+    ) {
+        this.id = id;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(
+            String type
+    ) {
+        this.type = type;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(
+            String name
+    ) {
+        this.name = name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(
+            String address
+    ) {
+        this.address = address;
+    }
+
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(
+            Double lat
+    ) {
+        this.lat = lat;
+    }
+
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(
+            Double lng
+    ) {
+        this.lng = lng;
+    }
+}
